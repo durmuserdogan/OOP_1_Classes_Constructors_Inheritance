3e6fdd5e3854dafef798adea2597f36143d83695
durmuserdogan <drmserdogan@outlook.com>
2022-08-11 16:44:56 +0300

    deneme

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(String[] args) {
+
+        Car porsche = new Car();
+        Car holden = new Car();
+        porsche.setModel("Carrera");
+        System.out.println("model is "+ porsche.getModel());
+
+
+    }
 }
