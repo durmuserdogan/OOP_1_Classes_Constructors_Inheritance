f97e4d1275c313f1d489c158ee92f8af7e2b52ca
durmuserdogan <drmserdogan@outlook.com>
2022-08-12 13:59:50 +0300

    Simple Calculator

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
+/*        // Car Class example
         Car porsche = new Car();
         Car holden = new Car();
-        porsche.setModel("Carrera");
-        System.out.println("model is "+ porsche.getModel());
+        porsche.setModel("PORSCHE");
+        System.out.println("model is "+ porsche.getModel());*/
+
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add= "+calculator.getAdditionResult());
+        System.out.println("substract= "+ calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply= "+calculator.getMultiplicationResult());
+        System.out.println("divide= "+calculator.getDivisionResult());
 
 
     }
