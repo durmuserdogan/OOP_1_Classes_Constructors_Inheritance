b019c3529e50031733bed63c8509e5f705f98047
durmuserdogan <drmserdogan@outlook.com>
2022-08-17 09:30:44 +0300

    Wall area

--- src/Main.java
+++ src/Main.java
@@ -28,6 +28,24 @@ public class Main {
         dumAccount.deposit(500.0);
         dumAccount.withdrawal(100.0);*/
 
+/*        //Vip Customer
+        VipCustomer person1= new VipCustomer();
+        System.out.println(person1.getName());
+
+        VipCustomer person2 = new VipCustomer("bob", 25000.00);
+        System.out.println(person2.getName());
+
+        VipCustomer person3=new VipCustomer("tim", 100.00, "tim@email.com");
+        System.out.println(person3.getName());*/
+
+        // Wall Area
+        Wall wall = new Wall(5,4);
+        System.out.println("area= " + wall.getArea());
+
+        wall.setHeight(-1.5);
+        System.out.println("width= "+ wall.getWidth());
+        System.out.println("height="+ wall.getHeight());
+        System.out.println("area= " + wall.getArea());
 
 
 
---
+++ src/Wall.java
@@ -0,0 +1,57 @@
+public class Wall {
+
+    private double width;
+    private double height;
+
+    public Wall() {
+        this(30,40);
+    }
+
+    public Wall(double width, double height) {
+        if(width<0){
+            this.width=0;
+        }
+        if(height<0){
+            this.height=0;
+        }
+        else{
+            this.width = width;
+            this.height = height;
+        }
+
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+        if(this.width<0){
+            this.width=0;
+        }
+        else{
+            this.width = width;
+        }
+
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+        if(this.height<0){
+            this.height=0;
+        }
+        else{
+            this.height = height;
+        }
+
+    }
+
+    public double getArea(){
+        return height*width;
+    }
+}
