1da554e531798cc79d517e667ff89dbcb0db44c0
durmuserdogan <drmserdogan@outlook.com>
2022-08-17 19:16:35 +0300

    Carpet Cost Calculator

--- src/Main.java
+++ src/Main.java
@@ -38,14 +38,26 @@ public class Main {
         VipCustomer person3=new VipCustomer("tim", 100.00, "tim@email.com");
         System.out.println(person3.getName());*/
 
-        // Wall Area
+/*        // Wall Area
         Wall wall = new Wall(5,4);
         System.out.println("area= " + wall.getArea());
 
         wall.setHeight(-1.5);
         System.out.println("width= "+ wall.getWidth());
         System.out.println("height="+ wall.getHeight());
-        System.out.println("area= " + wall.getArea());
+        System.out.println("area= " + wall.getArea());*/
+
+        // Carpet Cost Calculator
+        Carpet carpet = new Carpet(3.5);
+        Floor floor = new Floor(2.75, 4);
+        Calculator calculator = new Calculator(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
+        carpet = new Carpet(1.5);
+        floor = new Floor(5.4,4.5);
+        calculator = new Calculator(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
+
+
 
     }
 }
