288719d6bce955f774f176c2857732f4f99e16be
durmuserdogan <drmserdogan@outlook.com>
2022-08-18 16:20:20 +0300

    Complex operations

--- src/Main.java
+++ src/Main.java
@@ -47,7 +47,7 @@ public class Main {
         System.out.println("height="+ wall.getHeight());
         System.out.println("area= " + wall.getArea());*/
 
-        // Carpet Cost Calculator
+/*        // Carpet Cost Calculator
         Carpet carpet = new Carpet(3.5);
         Floor floor = new Floor(2.75, 4);
         Calculator calculator = new Calculator(floor, carpet);
@@ -55,7 +55,20 @@ public class Main {
         carpet = new Carpet(1.5);
         floor = new Floor(5.4,4.5);
         calculator = new Calculator(floor, carpet);
-        System.out.println("total= " + calculator.getTotalCost());
+        System.out.println("total= " + calculator.getTotalCost());*/
+
+        // Complex Operations
+        ComplexNumber one= new ComplexNumber(1.0,1.0);
+        ComplexNumber number = new ComplexNumber(2.5,-1.5);
+        one.add(1,1);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        one.subtract(number);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        number.subtract(one);
+        System.out.println("number.real= " + number.getReal());
+        System.out.println("number.imaginary= " + number.getImaginary());
 
 
 
