e98ebe3a517cfee9658dd369bcdc1e5ba590615f
durmuserdogan <drmserdogan@outlook.com>
2022-10-06 18:15:53 +0300

    Vehicle challenge

--- src/Main.java
+++ src/Main.java
@@ -70,13 +70,20 @@ public class Main {
         System.out.println("number.real= " + number.getReal());
         System.out.println("number.imaginary= " + number.getImaginary());*/
 
-        //Inheritance Part1 and Part2
+/*        //Inheritance Part1 and Part2
         Animal animal = new Animal("Animal ", 1,1,5,5);
         Dog dog = new Dog("basguda", 8, 20,2,4,1,20,"long silky");
         dog.eat();
 
         dog.walk();
-        dog.run();
+        dog.run();*/
+
+        // Vehicle Challenge ex.
+        Outlander outlander = new Outlander(36);
+        outlander.steer(45);
+        outlander.accelerate(30);
+        outlander.accelerate(20);
+        outlander.accelerate(-42);
 
 
     }
